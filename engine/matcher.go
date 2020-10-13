f27bf119125f7c1da0c7ee061e3165345608a04c
Francesco De Lisi <franxster@gmail.com>
2020-10-13 18:21:16 +0800

    Testing

---
+++ engine/matcher.go
@@ -0,0 +1,6 @@
+package engine
+
+// Test is just a test
+func Test() {
+	println("test")
+}
--- server.go
+++ server.go
@@ -1,29 +1,36 @@
 package main
 
-import (
-	"fmt"
-	"github.com/brbb/fast-topic-matching/matching"
-	"github.com/gorilla/mux"
-	"html"
-	"log"
-	"net/http"
-)
+import "github.com/brbb/fast-topic-matching/engine"
 
-var matcher = NewCSTrieMatcher()
+// import (
+// 	"fmt"
+// 	"github.com/brbb/fast-topic-matching/matching"
+// 	"github.com/gorilla/mux"
+// 	"html"
+// 	"log"
+// 	"net/http"
+// )
+
+// var matcher = NewCSTrieMatcher()
+
+// func main() {
+
+// 	router := mux.NewRouter()
+
+// 	router.HandleFunc("/subscribe", subscribe)
+// 	log.Fatal(http.ListenAndServe(":8080", router))
+// }
+
+// func subscribe(w http.ResponseWriter, r *http.Request) {
+// 	fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+// 	fascriber := Fascriber{WebSocket, "fas123"}
+// 	_, err := matcher.Subscribe("vehicle/motional0001/state", fascriber)
+// 	if err == nil {
+// 		fascriber.ConfirmSubscription()
+// 	}
+// }
 
 func main() {
 
-	router := mux.NewRouter()
-
-	router.HandleFunc("/subscribe", subscribe)
-	log.Fatal(http.ListenAndServe(":8080", router))
-}
-
-func subscribe(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
-	fascriber := Fascriber{WebSocket, "fas123"}
-	_, err := matcher.Subscribe("vehicle/motional0001/state", fascriber)
-	if err == nil {
-		fascriber.ConfirmSubscription()
-	}
+	println("Ciao")
 }
